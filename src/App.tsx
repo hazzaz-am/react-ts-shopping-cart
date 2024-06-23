bb9c7309ea3df7415c8035b07bd8e9c43080bd31
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-06-23 18:39:45 +0600

    routing setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
 
 const App = () => {
-  return (
-    <h1>Hi</h1>
-  )
-}
+	return (
+		<Container className="mb-4">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/store" element={<Store />} />
+			</Routes>
+		</Container>
+	);
+};
 
-export default App
+export default App;
