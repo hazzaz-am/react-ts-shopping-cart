92c12e562045188cf7559ee10d5620e0e867d853
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-06-24 10:11:39 +0600

    navbar design completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
+import Navbar from "./components/Navbar";
 
 const App = () => {
 	return (
-		<Container className="mb-4">
+		<>
+			<Navbar/>
+			<Container className="mb-4">
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/store" element={<Store />} />
 			</Routes>
 		</Container>
+		</>
 	);
 };
 
